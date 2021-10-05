17bffc80c5c73e41f5b892f8aca184c2d361df46
rocketyen <rocketyen@gmail.com>
2021-10-05 08:57:46 +0200

    commit

--- wp-content/themes/beer/archive.php
+++ wp-content/themes/beer/archive.php
@@ -1,25 +1,26 @@
 <?php get_header(); ?>
- 	<h1>Le blog d'Etienne</h1>
+<h1>Le blog d'Etienne</h1>
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-  
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 		<article class="post">
 			<h2><?php the_title(); ?></h2>
-      
-        	<?php the_post_thumbnail(); ?>
-            
-            <p class="post__meta">
-                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-                par <?php the_author(); ?> • <?php comments_number(); ?>
-            </p>
-            
-      		<?php the_excerpt(); ?>
-              
-      		<p>
-                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-            </p>
+
+			<?php the_post_thumbnail(); ?>
+
+			<p class="post__meta">
+				Publié le <?php the_time(get_option('date_format')); ?>
+				par <?php the_author(); ?> • <?php comments_number(); ?>
+			</p>
+
+			<?php the_excerpt(); ?>
+
+			<p>
+				<a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+			</p>
 		</article>
 
-	<?php endwhile; endif; ?>
-    <?php get_template_part( 'parts/newsletter' ); ?>
+<?php endwhile;
+endif; ?>
+<?php get_template_part('parts/newsletter'); ?>
 <?php get_footer(); ?>
--- wp-content/themes/beer/page.php
+++ wp-content/themes/beer/page.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    
-    	<h1><?php the_title(); ?></h1>
-    
-    	<?php the_content(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<?php endwhile; endif; ?>
+		<h1><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+
+<?php endwhile;
+endif; ?>
 
 <?php get_footer(); ?>
